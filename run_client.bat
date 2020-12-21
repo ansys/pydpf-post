b2bba34fb4da376e56a1fa03f399d281445bdafb
lparadis <lea.paradis@ansys.com>
2020-12-21 13:57:52 +0100

    run_client.bat remarks removed

--- run_client.bat
+++ run_client.bat
@@ -1,15 +1,7 @@
 
 @echo on
 
-rem set ANSYS_PATH=C:\Program Files\ANSYS Inc\v211
-rem set DPF_CONFIGURATION=release
-rem set DPF_PATH=C:\Program Files\ANSYS Inc\v211\aisol\bin\winx64\Ans.Dpf.Grpc.exe
-rem set ANS_PROTOCOL_ROOT=D:\AnsysDev\Protocols
-set DPF_CORE_PATH=D:\AnsysDev\DPF-Core
 set DPF_POST_PATH=D:\AnsysDev\DPF-Post
-
-rem %ANS_PROTOCOL_ROOT%\packages\python\dpf;
-
 set PYTHONPATH=%DPF_POST_PATH%
 
 set root=C:\ProgramData\Anaconda3
