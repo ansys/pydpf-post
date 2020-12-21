cda9570eb36c6928c811952eed90668e3ec673d0
lparadis <lea.paradis@ansys.com>
2020-12-21 11:50:05 +0100

    run_client.bat updated

--- run_client.bat
+++ run_client.bat
@@ -1,14 +1,16 @@
 
 @echo on
 
-set ANSYS_PATH=C:\Program Files\ANSYS Inc\v211\
-set DPF_CONFIGURATION=release
-set DPF_PATH=C:\Program Files\ANSYS Inc\v211\aisol\bin\winx64\Ans.Dpf.Grpc.exe
-set ANS_PROTOCOL_ROOT=D:\AnsysDev\Protocols
+rem set ANSYS_PATH=C:\Program Files\ANSYS Inc\v211
+rem set DPF_CONFIGURATION=release
+rem set DPF_PATH=C:\Program Files\ANSYS Inc\v211\aisol\bin\winx64\Ans.Dpf.Grpc.exe
+rem set ANS_PROTOCOL_ROOT=D:\AnsysDev\Protocols
 set DPF_CORE_PATH=D:\AnsysDev\DPF-Core
 set DPF_POST_PATH=D:\AnsysDev\DPF-Post
 
-set PYTHONPATH=%ANS_PROTOCOL_ROOT%\packages\python\dpf;%DPF_CORE_PATH%;%DPF_POST_PATH%
+rem %ANS_PROTOCOL_ROOT%\packages\python\dpf;
+
+set PYTHONPATH=%DPF_POST_PATH%
 
 set root=C:\ProgramData\Anaconda3
 call %root%\Scripts\activate.bat %root%
