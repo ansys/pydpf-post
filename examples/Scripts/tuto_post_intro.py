ec62ead67547b4f29d56d5f563b0ef9f3c90a11d
lparadis <lea.paradis@ansys.com>
2020-12-30 17:30:31 +0100

    script examples removed

--- examples/Scripts/tuto_post_intro.py
+++
@@ -1,6 +0,0 @@
-from ansys.dpf import post
-solution = post.load_solution("d:/rst/static.rst")
-print(solution)
-print(solution.get_result_info())
-help(post.locations)
-post.print_available_keywords()
