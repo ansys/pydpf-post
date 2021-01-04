0edf15328030f48c8775b6edcfc57e1e69368abb
lparadis <lea.paradis@ansys.com>
2021-01-04 16:14:06 +0100

    overview example modification

--- examples/overview/00-overview_example.py
+++ examples/overview/00-overview_example.py
@@ -10,13 +10,13 @@ can read the detailed tutorials.
 
 ###############################################################################
 # Start ansys.post module
-# ~~~~~~~~~~~~~~~~~~~~~~~
+# =======================
 # Here we load the post module.
 from ansys.dpf import post
 
 ###############################################################################
 # Prepare a solution object
-# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# =========================
 # We will first get the solution object that allows to access the result.
 # It must be instantiated with the result filepath. 
 import os
@@ -27,7 +27,7 @@ solution = post.load_solution(path)
 
 ###############################################################################
 # Get the mesh and the time_freq_support
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ======================================
 # The mesh is the support of the model. 
 # The time freq support is the time/frequence representation of the model.
 mesh = solution.mesh
@@ -35,24 +35,23 @@ time_freq_support = solution.time_freq_support
 
 ###############################################################################
 # Get a result object from the solution object
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ============================================
 # The result can be: stress, plastic_strain, elastic_strain, temperature, 
 # displacement). 
 post.print_available_keywords()
-stress = solution.stress(location=post.locations.elemental, named_selection="SELECTION", time_scoping=[1]) 
+stress = solution.stress(location=post.locations.nodal, time_scoping=[1]) 
 print(stress)
 
 stress.definition.location
-stress.definition.named_selection
 stress.definition.time_scoping
 
 ###############################################################################
 # Compute data
-# ~~~~~~~~~~~~
-# Get a subresult (such as SX, or the stress tensor)
+# ============
+# Here is shown how to get a subresult (such as SX, or the stress tensor).
 
 ###############################################################################
-# SX subresult
+# **SX subresult**
 sx = stress.xx
 sx.num_fields
 sx_field = sx[0]
@@ -63,7 +62,7 @@ sx.max_data
 sx.get_max_data_at_field(0)
 
 ###############################################################################
-# Stress tensor result
+# **Stress tensor result**
 s = stress.tensor
 s_field = s[0]
 s_data = sx.get_data_at_field(0)
@@ -78,7 +77,7 @@ s.get_min_data_at_field(0)
 
 ###############################################################################
 # Display a result
-# ~~~~~~~~~~~~~~~~
+# ================
 # Use <subresult>.plot_contour() to plot a result. 
 s.plot_contour()
 
