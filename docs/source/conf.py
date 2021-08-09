4422ce330afc4a876f1fd845fa98a4f0159b8c7e
anslpa <74727146+anslpa@users.noreply.github.com>
2021-08-09 11:20:20 +0200

    Documentation 0.2.0 updated (#24)
    * Fix documentation getting stated page
    
    * UpdateDPF-Post documentation themes
    
    * Publishes documentation updated
    
    * Logo fix
    
    * Condition for documentation publication reset
    
    * requirements_docs.txt updated (pydata-theme not used anymore, dashes used) and show_prev_next used
    
    Co-authored-by: Lea Paradis <lparadis@lyoc7mbudev1.ansys.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,29 +115,21 @@ sphinx_gallery_conf = {
 # a list of builtin themes.
 #
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'pydata_sphinx_theme'
+# html_theme = 'pydata_sphinx_theme'
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # zerovm-sphinx-theme
 
+html_theme = 'pyansys_sphinx_theme'
 html_logo = '_static/pyansys-logo-black-cropped.png'
-
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
 html_theme_options = {
-    "github_url": "https://github.com/pyansys/DPF-Core",
-    # "show_toc_level": 1,
-    # "show_prev_next": False
+    "github_url": "https://github.com/pyansys/DPF-Post",
+    "show_prev_next": False,
 }
-
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = ['css/ansys.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
--- docs/source/getting_started/index.rst
+++ docs/source/getting_started/index.rst
@@ -3,7 +3,7 @@ Getting Started
 ===============
 To use the DPF-Post API, you need to have the DPF-Core module installed.
 You also need to use the native DPF server, you need to have a local installation of
-Ansys 2021R1.  Visit https://www.ansys.com/ for more information on
+Ansys 2021R1 or newer.  Visit https://www.ansys.com/ for more information on
 getting a licensed copy of Ansys.
 
 
@@ -16,7 +16,7 @@ getting a licensed copy of Ansys.
 
 Compatibility
 ~~~~~~~~~~~~~
-As of 20201R1, DPF-Post supports Windows 10 and CentOS 7 and newer.  For
+DPF-Post supports Windows 10 and CentOS 7 and newer. For
 more details, see `Ansys Platform Support <https://www.ansys.com/solutions/solutions-by-role/it-professionals/platform-support>`_.
 
 Other platforms may be supported by using DPF-Post within a
