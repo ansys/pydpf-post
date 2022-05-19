64117835f3210ab1c739c3bca8d8c02f4f28f303
paul.profizi <paul.profizi@ansys.com>
2022-05-19 14:34:01 +0200

    Removing azure pipeline files

--- .ci/build_doc.bat
+++
@@ -1,8 +0,0 @@
-ECHO %AWP_ROOT221%
-set SPHINX_APIDOC_OPTIONS=inherited-members
-call sphinx-apidoc -o ../docs/source/api ../ansys -f --implicit-namespaces --separate  --no-headings
-pushd .
-cd ../docs/
-call make clean
-call make html
-popd
