9612a74fe7897cbb6ffaefd1e90cc19ad4bcae12
PProfizi <100710998+PProfizi@users.noreply.github.com>
2022-06-30 18:50:30 +0200

    Temporary restrain protobuf to 3.20.1
    Temporary fix due to breaking change with protobuf >=4.*

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from io import open as io_open
 from setuptools import setup
 
-install_requires = ["ansys.dpf.core>=0.3.0", "scooby"]
+install_requires = ["ansys.dpf.core>=0.3.0", "scooby", "protobuf<=3.20.1"]
 
 
 # Get version from version info
