774f42ac9790589de23bf71ab8a1c8a8fd422c21
Alex Kaszynski <akascap@gmail.com>
2022-08-18 13:50:39 -0600

    fix python_requires

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    python_requires=">=3.7.*,!=3.10.*",
+    python_requires=">=3.7.*,<4.0",
     extras_require={
         "plotting": ["pyvista>=0.24.0", "matplotlib"],
     },
