7c0d3715097dd2dc640368ade55ec70b98613573
Alex Kaszynski <akascap@gmail.com>
2022-08-18 19:51:36 -0600

    conditionally disable lighting

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@
 Launch or connect to a persistent local DPF service to be shared in
 pytest as a sesson fixture
 """
+import re
 import os
 
 import pytest
@@ -15,6 +16,28 @@ from ansys.dpf.post import examples
 # enable off_screen plotting to avoid test interruption
 pv.OFF_SCREEN = True
 mpl.use("Agg")
+
+
+def get_lighting():
+    """Get lighting configuration.
+
+    Disable lighting when using OSMesa on Windows. See:
+    https://github.com/pyvista/pyvista/issues/3185
+
+    """
+    pl = pv.Plotter(notebook=False, off_screen=True)
+    pl.add_mesh(pv.Sphere())
+    pl.show(auto_close=False)
+    gpu_info = pl.ren_win.ReportCapabilities();
+    pl.close()
+
+    regex = re.compile("OpenGL version string:(.+)\n")
+    version = regex.findall(gpu_info)[0]
+    return not(os.name == 'nt' and 'Mesa' in version)
+
+
+pv.global_theme.lighting = use_lighting()
+
 
 # currently running dpf on docker.  Used for testing on CI
 running_docker = os.environ.get("DPF_DOCKER", False)
