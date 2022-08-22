a214c7349cf4b65ba1336a6a74927c95810497b6
Alex Kaszynski <akascap@gmail.com>
2022-08-22 08:53:28 -0600

    rearrange import order

--- src/ansys/dpf/post/__init__.py
+++ src/ansys/dpf/post/__init__.py
@@ -16,20 +16,19 @@ Examples
 import ansys.dpf.core as core
 from ansys.dpf.core.common import locations
 
-from ansys import dpf
+from ansys.dpf.post.common import Grouping as grouping
+from ansys.dpf.post.dpf_path import create_path_on_coordinates
+from ansys.dpf.post.misc import Report
+from ansys.dpf.post.post_utility import load_solution, print_available_keywords
 
 try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:  # pragma: no cover
     import importlib_metadata
 
+# this must be after some ansys.dpf.post import
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
-
-from ansys.dpf.post.common import Grouping as grouping
-from ansys.dpf.post.dpf_path import create_path_on_coordinates
-from ansys.dpf.post.misc import Report
-from ansys.dpf.post.post_utility import load_solution, print_available_keywords
 
 if hasattr(core, "settings") and hasattr(
     core.settings, "set_dynamic_available_results_capability"
