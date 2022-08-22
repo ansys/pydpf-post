30fe3810e994e6c33d2c8d8a21438866058dae06
Alex Kaszynski <akascap@gmail.com>
2022-08-22 09:32:23 -0600

    order back

--- src/ansys/dpf/post/__init__.py
+++ src/ansys/dpf/post/__init__.py
@@ -9,17 +9,12 @@ Examples
 >>> from ansys.dpf import post
 >>> from ansys.dpf.post import examples
 >>> solution = post.load_solution(examples.static_rst)
->>> disp = solution.nodal_displacement()
+>>> disp = solution.displacement()
 
 """
 
 import ansys.dpf.core as core
 from ansys.dpf.core.common import locations
-
-from ansys.dpf.post.common import Grouping as grouping
-from ansys.dpf.post.dpf_path import create_path_on_coordinates
-from ansys.dpf.post.misc import Report
-from ansys.dpf.post.post_utility import load_solution, print_available_keywords
 
 try:
     import importlib.metadata as importlib_metadata
@@ -29,6 +24,10 @@ except ModuleNotFoundError:  # pragma: no cover
 # this must be after some ansys.dpf.post import
 __version__ = importlib_metadata.version("ansys-dpf-post")
 
+from ansys.dpf.post.common import Grouping as grouping
+from ansys.dpf.post.dpf_path import create_path_on_coordinates
+from ansys.dpf.post.misc import Report
+from ansys.dpf.post.post_utility import load_solution, print_available_keywords
 
 if hasattr(core, "settings") and hasattr(
     core.settings, "set_dynamic_available_results_capability"
