9edec9f613c9b888723f1bf6e6cd354f64da9841
paul.profizi <paul.profizi@ansys.com>
2022-09-16 09:51:44 +0200

    WIP: problem found with mesh data not held.

--- examples/03-Plotting/02-animations.py
+++ examples/03-Plotting/02-animations.py
@@ -11,10 +11,33 @@ from ansys.dpf.post import examples
 
 # Load the solution
 solution = post.load_solution(examples.msup_transient)
-
+print(solution)
+print("==============================")
 # Get the ResultData for displacement
-displacement_result = solution.displacement()
-displacement = displacement_result.vector
+print(post.print_available_keywords())
+print("==============================")
+displacement_result = solution.displacement()  # node_scoping=range(1, 394))
+displacement = displacement_result.x
 
-# Create an animation using the animate method
-displacement.animate()
+print(displacement)
+fc = displacement.result_fields_container
+print(fc)
+field = fc[0]
+print(field)
+mesh = field.meshed_region
+print(mesh)
+print("====")
+print(mesh.nodes)
+print(mesh.nodes.scoping)
+# print(mesh.nodes.scoping.ids)
+# scoping = mesh.nodes.scoping
+# print(scoping)
+# print(scoping.ids)
+
+# # Create an animation using the animate method
+# displacement.animate()
+#
+# stress_result = solution.stress()
+# stress = stress_result.von_mises
+#
+# stress.animate()
