d9e647a82da8710a15bd8f49dced31042393b8fe
guillem.barroso <guillem.barroso@ansys.com>
2022-12-28 16:35:01 +0100

    Improve testing

--- tests/test_data_object.py
+++ tests/test_data_object.py
@@ -1,46 +1,90 @@
+import pytest
+
 from ansys.dpf.post import examples, load_simulation
 
 
-def test_get_displacements():
-    simulation = load_simulation(examples.download_transient_result())
+@pytest.mark.parametrize("example", ["allkindofcomplexity", "simple_bar"])
+def test_get_displacements(example, request):
+    simulation = load_simulation(request.getfixturevalue(example))
+    # simulation = load_simulation(example)
+
+    for field in simulation.results:
+        if field.name == "displacement":
+            n_dim = field.n_components
+            break
+
+    n_time_steps = len(simulation.time_freq_support.time_frequencies.data)
+    # n_nodes = simulation.mesh._meshed_region.nodes.n_nodes
+
     disp = simulation.displacement()
-    assert len(disp) == 35
-    assert disp[0].ndim == 3
-    assert disp[0].elementary_data_count == 3820
+    assert len(disp) == n_time_steps
+    assert disp[0].ndim == n_dim
+    # assert disp[0].elementary_data_count == n_nodes # HOW CAN WE CHECK THE n_entities?
     disp_x = simulation.displacement(component=0)
-    assert len(disp_x) == 35
+    assert len(disp_x) == n_time_steps
     assert disp_x[0].ndim == 1
-    assert disp[0].elementary_data_count == 3820
-    disp_time = simulation.displacement(steps=[1, 25])
-    assert len(disp_time) == 2
-    assert disp_time[0].elementary_data_count == 3820
+    # assert disp[0].elementary_data_count == n_nodes # HOW CAN WE CHECK THE n_entities?
+
+    if n_time_steps > 1:
+        disp_time = simulation.displacement(steps=[1, 25])
+        assert len(disp_time) == 2
+        assert disp[0].ndim == n_dim
+        # assert disp_time[0].elementary_data_count == n_nodes # HOW CAN WE CHECK THE n_entities?
 
 
-def test_get_stresses():
-    simulation = load_simulation(examples.download_transient_result())
+def test_get_stresses(allkindofcomplexity, static_rst):
+    ###########################################################################
+    # allkindofcomplexity example
+    simulation = load_simulation(allkindofcomplexity)
 
+    for field in simulation.results:
+        if field.name == "stress":
+            n_dim = field.n_components
+            break
+
+    # n_elems = simulation.mesh._meshed_region.elements.n_elements
+    # n_nodes = simulation.mesh._meshed_region.nodes.n_nodes
+    n_stress_bodies = 2
     # Nodal stress
     stress = simulation.nodal_stress()
-    assert len(stress) == 35
-    assert stress[0].ndim == 6
-    assert stress[0].elementary_data_count == 3812
+    assert len(stress) == n_stress_bodies
+    assert stress[0].ndim == n_dim
+
     stress_xy_nodes = simulation.nodal_stress(
         component="XY", nodes=[100, 101, 102, 103]
     )
-    assert len(stress_xy_nodes) == 35
+    assert len(stress_xy_nodes) == 1
     assert stress_xy_nodes[0].ndim == 1
     assert stress_xy_nodes[0].elementary_data_count == 4
 
     # Elemental stress
-    stress = simulation.elemental_stress(steps=[2, 15])
-    assert len(stress) == 2
-    assert stress[0].ndim == 6
-    assert stress[0].elementary_data_count == 715
+    stress = simulation.elemental_stress()
+    assert len(stress) == n_stress_bodies
+    assert stress[0].ndim == n_dim
 
     # Raw stress
     stress = simulation.raw_stress()
-    assert len(stress) == 35
-    assert stress[0].ndim == 6
-    assert (
-        stress[0].elementary_data_count == 5720
-    )  # why the number of entities is now different from data_count?
+    assert len(stress) == 1
+    # WHERE CAN WE GET BETTER INFO ON WHAT'S ON EACH ENTRY OF THE FIELDSCONTAINER?
+    assert stress[0].ndim == n_dim
+
+    ###########################################################################
+    # static_rst example
+    simulation = load_simulation(static_rst)
+
+    for field in simulation.results:
+        if field.name == "stress":
+            n_dim = field.n_components
+            break
+
+    stress = simulation.nodal_stress()
+    assert len(stress) == 1
+    assert stress[0].ndim == n_dim
+
+    stress = simulation.elemental_stress()
+    assert len(stress) == 1
+    assert stress[0].ndim == n_dim
+
+
+if __name__ == "__main__":
+    test_get_stresses(examples.download_all_kinds_of_complexity(), examples.static_rst)
