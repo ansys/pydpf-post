075dafd58c08e4a8c43a5475134bc458eb1f1d7f
guillem.barroso <guillem.barroso@ansys.com>
2022-12-28 17:33:08 +0100

    Add new Mesh properties to tests

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -6,15 +6,25 @@ from ansys.dpf import post
 
 @pytest.fixture()
 def simulation(allkindofcomplexity):
-    return post.load_solution(allkindofcomplexity, legacy=False)
+    return post.load_simulation(allkindofcomplexity)
 
 
 def test_mesh(simulation):
-    assert type(simulation.mesh._meshed_region) == MeshedRegion
-    assert len(simulation.mesh._meshed_region.nodes.scoping) >= 15000
+    mesh = simulation.mesh
+    assert type(mesh._meshed_region) == MeshedRegion
+    assert len(mesh._meshed_region.nodes.scoping) >= 15000
 
-
-def test_available_named_selections(simulation):
     available_named_selections = simulation.mesh.available_named_selections
     assert len(available_named_selections) == 6
     assert "_ELMISC" in available_named_selections
+
+    available_property_fields = mesh.available_property_fields
+    assert len(available_property_fields) == 6
+    assert "connectivity" in available_property_fields
+
+    assert mesh.grid.n_cells == 10292
+    assert mesh.grid.n_points == 15129
+
+    assert mesh.elements.n_elements == 10292
+    assert mesh.nodes.n_nodes == 15129
+    assert mesh.unit == "m"
