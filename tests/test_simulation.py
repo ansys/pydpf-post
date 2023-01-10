049092f2949f2cb0178ab6c57494e949ed350167
paul.profizi <paul.profizi@ansys.com>
2023-01-10 18:30:28 +0100

    Add test_simulation.py

---
+++ tests/test_simulation.py
@@ -0,0 +1,41 @@
+from pytest import fixture
+
+import ansys.dpf.post as dpf
+from ansys.dpf.post.mesh import Mesh
+
+
+@fixture
+def simulation(simple_bar):
+    return dpf.load_simulation(data_sources=simple_bar)
+
+
+def test_simulation_results(simulation):
+    results = simulation.results
+    assert len(results) == 10
+    assert all(isinstance(x, str) for x in results)
+
+
+def test_simulation_geometries(simulation):
+    geometries = simulation.geometries
+    assert geometries == []
+
+
+def test_simulation_boundary_conditions(simulation):
+    boundary_conditions = simulation.boundary_conditions
+    assert boundary_conditions == []
+
+
+def test_simulation_loads(simulation):
+    loads = simulation.loads
+    assert loads == []
+
+
+def test_simulation_mesh(simulation):
+    mesh = simulation.mesh
+    assert isinstance(mesh, Mesh)
+
+
+def test_simulation_named_selections(simulation):
+    named_selections = simulation.named_selections
+    assert len(named_selections) == 1
+    assert all(isinstance(x, str) for x in named_selections)
