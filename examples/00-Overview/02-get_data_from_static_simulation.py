3336512dc6dc5099cec8e1f95d6a5c553c88069b
paul.profizi <paul.profizi@ansys.com>
2023-01-25 16:37:51 +0100

    Add static_mechanical overview example from Guillem's PR

---
+++ examples/00-Overview/02-get_data_from_static_simulation.py
@@ -0,0 +1,99 @@
+"""
+.. _ref_get_data_from_static_simulation:
+
+Get data from static simulation
+===============================
+This example shows how to request data from a previously stored static simulation.
+The available results can be listed to see what results can be retrieved.
+"""
+
+###############################################################################
+# Imports and loading simulation
+# ------------------------------
+from ansys.dpf.post import examples, load_simulation
+
+simulation = load_simulation(examples.download_all_kinds_of_complexity())
+
+###############################################################################
+# Get and plot displacements
+# --------------------------
+displacement = simulation.displacement()
+
+###############################################################################
+# Print information
+print(displacement)
+
+###############################################################################
+# Plot displacements
+displacement[0].plot()
+
+###############################################################################
+# Get and plot stresses
+# ---------------------
+# Request "XY" stress component
+stress = simulation.nodal_stress(component="XY")
+
+###############################################################################
+# Print information
+print(stress)
+
+###############################################################################
+# Plot available stresses. From the print above it can be seen that the two
+# stress fields available represent the stress for a single time = 1 in two different
+# parts of the structure, denoted with the label ``elshape = 1`` and ``elshape = 2``.
+stress[0].plot()
+stress[1].plot()
+
+###############################################################################
+# Get stresses in only 100 nodes
+# ------------------------------
+# Request stress only in 100 nodes by its ID
+stress_nodes = simulation.nodal_stress(nodes=range(400, 500))
+
+###############################################################################
+# Print information
+print(stress_nodes)
+
+###############################################################################
+# Plot stresses
+stress_nodes[0].plot()
+
+###############################################################################
+# Get stresses in a named selection
+# ---------------------------------
+# Request stress in named selection "_CM82"
+stress_named_sel = simulation.nodal_stress(named_selection="_CM82")
+
+###############################################################################
+# Print information
+print(stress_named_sel)
+
+###############################################################################
+# Plot stresses
+stress_named_sel[0].plot()
+
+###############################################################################
+# Get stresses in a few elements
+# ------------------------------
+# Request stress only for a few elements selected by their ID
+stress_elements = simulation.nodal_stress(elements=[301, 302, 303])
+
+###############################################################################
+# Print information
+print(stress_elements)
+
+###############################################################################
+# Plot stresses
+stress_elements[0].plot()
+
+###############################################################################
+# Get elemental stress and raw stresses
+# -------------------------------------
+# Request elemental stresses and print information
+stress_elements = simulation.elemental_stress()
+print(stress_elements)
+
+###############################################################################
+# Request raw stresses ("ElementalNodal") and print information
+stress_raw = simulation.raw_stress()
+print(stress_raw)
