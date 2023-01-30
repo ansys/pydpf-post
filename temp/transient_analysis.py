b122bda18a490895ec5ad89903565ee486b8a977
paul.profizi <paul.profizi@ansys.com>
2023-01-30 14:56:30 +0100

    Add main case of Selection creation via tools.create_selection

--- temp/transient_analysis.py
+++ temp/transient_analysis.py
@@ -76,6 +76,11 @@ load.interpolate(mesh=another_mesh)
 
 # -----------------------------------------------------------------------------------------
 # Define and use a selection
+# Using the provided factories:
+from ansys.dpf.post import tools
+
+selection = tools.create_selection(nodes=[1, 2, 3], elements=[1, 2, 3], steps=[1])
+# or
 selection = Selection(nodes=[1, 2, 4], time_freq_indices=[0, 1])
 selection = Selection()
 selection.nodes(nodes=[1, 2, 3])
