50ca3094a2dc5c915b9ebc9c03c33d25b0bd5688
Camille Bellot <80476446+cbellot000@users.noreply.github.com>
2023-03-02 18:25:55 +0100

    Update src/ansys/dpf/post/examples/__init__.py

--- src/ansys/dpf/post/examples/__init__.py
+++ src/ansys/dpf/post/examples/__init__.py
@@ -2,7 +2,7 @@
 import os
 
 # alias files used by the core
-from ansys.dpf.core.examples import *
+from ansys.dpf.core.examples import *  # noqa: F401
 
 # must copy files over when using docker
 # running_docker = os.environ.get('DPF_DOCKER', False)
