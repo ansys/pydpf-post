bfbc7e67c6f8b6443a15a982edc52661c29e1dbe
paul.profizi <paul.profizi@ansys.com>
2023-03-02 17:36:38 +0100

    Add truncation to print

--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -20,6 +20,12 @@ def df(static_rst):
     return simulation.displacement()
 
 
+@fixture
+def elastic_strain_df(static_rst):
+    simulation = StaticMechanicalSimulation(static_rst)
+    return simulation.elastic_strain_nodal()
+
+
 def test_dataframe_core_object(df):
     assert isinstance(df._core_object, core.FieldsContainer)
     assert len(df._core_object) == 1
@@ -115,7 +121,7 @@ def test_dataframe_repr(df):
     ref = (
         "DataFrame<index=MultiIndex<[MeshIndex<name=\"node\", dtype=<class 'int'>>, "
         "Index<name=\"comp\", dtype=<class 'str'>>]>, columns=MultiIndex<[ResultIndex<['U']>, "
-        "Index<name=\"set_id\", dtype=<class 'int'>>]>>"
+        "SetIndex<values=[1]>]>>"
     )
     assert repr(df) == ref
 
@@ -125,15 +131,16 @@ def test_dataframe_str(transient_rst):
     df = simulation.displacement(all_sets=True)
     print(df)
     ref = """
-             results         U                                                  
-              set_id         1         2         3         4         5         6
-      node      comp                                                            
-       525         X  0.00e+00  4.85e-05  2.30e-04  6.51e-04  1.48e-03  2.93e-03
-                   Y  0.00e+00  2.87e-04  1.14e-03  2.54e-03  4.41e-03  6.59e-03
-                   Z  0.00e+00 -1.26e-10 -4.34e-10 -8.29e-10 -1.15e-09 -1.39e-09
-       534         X  0.00e+00  6.55e-06  1.05e-04  5.30e-04  1.67e-03  4.02e-03
-                   Y  0.00e+00  6.27e-04  2.51e-03  5.62e-03  9.86e-03  1.50e-02
-                   Z  0.00e+00 -3.20e-10 -1.10e-09 -2.13e-09 -2.94e-09 -3.57e-09
+             results         U                                                         ...
+              set_id         1         2         3         4         5         6       ...
+      node      comp                                                                   ...
+       525         X  0.00e+00  4.85e-05  2.30e-04  6.51e-04  1.48e-03  2.93e-03       ...
+                   Y  0.00e+00  2.87e-04  1.14e-03  2.54e-03  4.41e-03  6.59e-03       ...
+                   Z  0.00e+00 -1.26e-10 -4.34e-10 -8.29e-10 -1.15e-09 -1.39e-09       ...
+       534         X  0.00e+00  6.55e-06  1.05e-04  5.30e-04  1.67e-03  4.02e-03       ...
+                   Y  0.00e+00  6.27e-04  2.51e-03  5.62e-03  9.86e-03  1.50e-02       ...
+                   Z  0.00e+00 -3.20e-10 -1.10e-09 -2.13e-09 -2.94e-09 -3.57e-09       ...
+       ...
 """  # noqa: W291
     assert str(df) == ref
     df = simulation.stress()
@@ -149,6 +156,7 @@ def test_dataframe_str(transient_rst):
               XY (1) -4.89e+06
               YZ (1)  1.43e+07
               XZ (1)  1.65e+07
+       ...
 """  # noqa: W291
     assert str(df) == ref
     df2 = df.select(comp="YY")
@@ -163,5 +171,34 @@ def test_dataframe_str(transient_rst):
        391    YY (4) -2.72e+07
        391    YY (5)  2.83e+07
        391    YY (6)  5.26e+06
+       ...
 """  # noqa: W291
     assert str(df2) == ref
+
+
+def test_dataframe_str_comp(df):
+    # 3D str
+    stri = str(df)
+    expected_strs = ["X", "Y", "Z", "results", "U", "node"]
+    for expected_str in expected_strs:
+        assert expected_str in stri
+    # 1D str
+    stri = str(df.select(comp="X"))
+    expected_strs = ["X", "results", "U", "node"]
+    for expected_str in expected_strs:
+        assert expected_str in stri
+    assert "Y" not in stri
+
+
+def test_dataframe_str_tensor(elastic_strain_df):
+    # 3D str
+    stri = str(elastic_strain_df)
+    expected_strs = ["XX", "XY", "XZ", "results", "EPEL", "node"]
+    for expected_str in expected_strs:
+        assert expected_str in stri
+    # 1D str
+    stri = str(elastic_strain_df.select(comp=["XX", "XY"]))
+    expected_strs = ["XX", "XY", "results", "EPEL", "node"]
+    for expected_str in expected_strs:
+        assert expected_str in stri
+    assert "ZZ" not in stri
