1967a0fffed7e16886075ff78b4871c64909e1e1
paul.profizi <paul.profizi@ansys.com>
2023-03-08 10:44:09 +0100

    Add run_examples step to CI

---
+++ .ci/run_examples.py
@@ -0,0 +1,28 @@
+"""Run all examples and check for success."""
+import glob
+import os
+import pathlib
+import subprocess
+import sys
+
+os.environ["PYVISTA_OFF_SCREEN"] = "true"
+os.environ["MPLBACKEND"] = "Agg"
+
+actual_path = pathlib.Path(__file__).parent.absolute()
+print(os.path.join(actual_path, os.path.pardir, "examples"))
+
+
+for root, subdirectories, files in os.walk(
+    os.path.join(actual_path, os.path.pardir, "examples")
+):
+    for subdirectory in subdirectories:
+        subdir = os.path.join(root, subdirectory)
+        for file in glob.iglob(os.path.join(subdir, "*.py")):
+            print("\n--------------------------------------------------")
+            print(file)
+            try:
+                subprocess.check_call([sys.executable, file])
+            except subprocess.CalledProcessError as e:
+                sys.stderr.write(str(e.args))
+                raise e
+            print("PASS")
