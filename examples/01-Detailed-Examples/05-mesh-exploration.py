0bc4c45640775fc926bec2c09cefd4d7b0f017e5
paul.profizi <paul.profizi@ansys.com>
2023-03-20 11:14:58 +0100

    Meeting 20/03

--- examples/01-Detailed-Examples/05-mesh-exploration.py
+++ examples/01-Detailed-Examples/05-mesh-exploration.py
@@ -34,13 +34,81 @@ print(simulation)
 # Get the mesh
 # ------------
 
-mesh = simulation.mesh
-print(mesh)
-# meshes : MeshesContainer = simulation.meshes_with_elements_grouped_by("mat_id")
-# meshes : MeshesContainer = simulation.meshes(elements_grouped_by="mat_id")
-# meshes : MeshesContainer = simulation.meshes.elements_grouped_by("mat_id")
-# meshes : MeshesContainer = simulation.meshes.elements_grouped_by("mat_id")
-# meshes : MeshesContainer = simulation.meshes_by_properties("mat_id", "thickness")
+# Use-case: big mesh, split by mat_id and thickness, get meshes for mat_id 1, limited evaluations
+
+# print(simulation.mesh_info) -> Queries metadata
+# ...
+# Available properties/labels: "mat_id", "thickness", "eltype", "elshape"
+# ...
+
+
+# Mesh object holds on a MeshesContainer, specify splits and necessary evaluation defined at query
+# Specific sub-mesh queries on Mesh by property
+
+# Proposal 1: Mesh wraps a MeshesContainer - simulation.mesh getter
+# meshes : Meshes = simulation.mesh(split_by="mat_id")
+# meshes : Meshes = simulation.mesh(split_by=["mat_id", "thickness"])
+
+# Get mesh split by thickness and mat_id but only for mat_id in 1, 2, 3
+# mesh : Mesh = simulation.mesh(mat_id=[1, 2, 3], split_by=["mat_id", "thickness"])
+# print(mesh)
+# mesh : Mesh = simulation.mesh(mat_id=[1, 2, 3], thickness=)
+
+# Pros:
+# - Not lazy but very targeted evaluations -> same logic as result queries
+# - simulation.mesh gets you a mesh/holder on meshes
+# Cons:
+# - simulation.mesh() by default would query everything as a single mesh?
+# - not a property
+
+
+# si
+
+
+# simulation.mesh -> Mesh
+# simulation.mesh.nodes_ids
+
+
+# Proposal 2: Mesh wraps a MeshesContainer - no simulation.mesh, only query methods
+# mesh_info : str = simulation.mesh_info
+
+# Get the whole mesh
+# mesh : Mesh = simulation.mesh (property)
+# meshes : Meshes = mesh.split_by()
+
+# Get the mesh split by material ID
+# meshes : Meshes = simulation.split_mesh_by("mat_id", "thickness")  <--
+
+
+# Get the mesh split by material ID but only for mat_id 1 and 2 and thickness 2
+# meshes : Meshes = simulation.split_mesh_by(labels={"mat_id": [1, 2], "elshape": })  <--
+# mesh = meshes.select(mat_id=1)
+# simulation.create_elemental_named_selection() -> Selection
+# simulation.create_named_selection(element_ids=[,2,,4], name="my_ns") -> Selection
+# simulation.split_mesh_by({"named_selection"="my_ns")
+# simulation.save_hfd5()
+# mesh.name = "mat_id 1"
+
+
+# print(meshes)
+# """
+# mesh1: {mat_id=1, thickness=2}
+# mesh1: {mat_id=2, thickness=2}
+# """
+
+
+# print(meshes)
+# """
+# mesh0: {mat_id=1}
+# mesh1: {mat_id=2}
+# """
+# Pros:
+# - explicit
+# Cons:
+
+
+# Proposal 3: simulation.mesh is a "method" holder, not an object
+# meshes : Mesh = simulation.mesh.get_mesh_split_by_property("mat_id", "thickness")
 # print(meshes)
 # """
 # mesh0: {mat_id=1, thickness=1}
@@ -48,10 +116,33 @@ print(mesh)
 # mesh2: {mat_id=2, thickness=1}
 # mesh3: {mat_id=2, thickness=2}
 # """
-# mesh = simulation.mesh_by_property(mat_id=1)
+
+# Cons:
+# simulation.mesh does not return anything
+
+
+mesh = simulation.mesh
+print(mesh)
+# meshes : Meshes = simulation.meshes_with_elements_grouped_by("mat_id")
+
+# meshes : Meshes = simulation.meshes(elements_grouped_by="mat_id")
+
+# meshes : Meshes = simulation.meshes.elements_grouped_by("mat_id")
+
+# meshes : Meshes = simulation.meshes_by_properties("mat_id", "thickness")
+# print(meshes)
+# """
+# mesh0: {mat_id=1, thickness=1}
+# mesh1: {mat_id=1, thickness=2}
+# mesh2: {mat_id=2, thickness=1}
+# mesh3: {mat_id=2, thickness=2}
+# """
+# mesh : Mesh = simulation.mesh_by_property(mat_id=1)
 # mesh_info = simulation.mesh_info ->
 # mesh = MeshesContainer.mesh_by_id(mat_id=1)
 # for id, mesh for
+
+
 ###############################################################################
 # Query basic information about the mesh (available)
 # --------------------------------------
