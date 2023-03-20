6001265be658b442c5c259cc5c5808eefc42f294
paul.profizi <paul.profizi@ansys.com>
2023-03-20 11:50:49 +0100

    Working 06-cyclic-results.py

--- examples/01-Detailed-Examples/06-cyclic-results.py
+++ examples/01-Detailed-Examples/06-cyclic-results.py
@@ -24,51 +24,39 @@ from ansys.dpf.post import examples
 # ``"C:/Users/user/my_result.rst"`` on Windows or ``"/home/user/my_result.rst"``
 # on Linux.
 
-example_path = examples.download_multi_stage_cyclic_result()
+example_path = examples.simple_cyclic
 simulation = post.ModalMechanicalSimulation(example_path)
 
 # print the simulation to get an overview of what's available
-print(simulation.mesh._core_object)
-# simulation.mesh._core_object.plot()
+print(simulation)
+
 #############################################################################
-# Extract expanded X displacement for the first mode
-# --------------------------------------------------
+# Extract expanded displacement norm
+# ----------------------------------
 
-expand_cyclic_arg = [
-    False,
-    True,
-    # [1, 2, 3],
-    # [[1, 2, 3], [1, 2, 3, 4]]
-]
+displacement_norm = simulation.displacement(
+    norm=True,
+    expand_cyclic=True,
+)
+print(displacement_norm)
+displacement_norm.plot()
 
+#############################################################################
+# Extract equivalent von mises nodal stress expanded on the first three sectors
+# -----------------------------------------------------------------------------
 
-for expand_cyclic in expand_cyclic_arg:
-    displacement = simulation.stress_nodal(
-        components="XX",
-        expand_cyclic=expand_cyclic,
-        phase_angle_cyclic=90.0,
-    )
-    fc = displacement._core_object
-    f = fc[0]
-    print("================")
-    print(f"{expand_cyclic=}")
-    print(f"{displacement}")
-    # print(f"{fc}")
-    # print(f"{f}")
-    # print(f"{f.meshed_region}")
-    # print(f"{f.scoping.location=}")
-    # print(f"{f.scoping.size=}")
-    # print(f"{f.meshed_region.elements.n_elements=}")
-    # print(f"{f.meshed_region.nodes.n_nodes=}")
-    displacement.plot()
-    # plt = DpfPlotter()
-    # try:
-    #     plt.add_field(field=f)
-    # except:
-    #     plt.add_field(field=f, meshed_region=simulation.mesh._core_object)
-    # plt.show_figure()
-# print(displacement._core_object)
-# print(displacement._core_object[0].meshed_region)  # Empty for read_cyclic=1
-# displacement._core_object[0].meshed_region.plot()
+stress_vm_1_2_3 = simulation.stress_eqv_von_mises_nodal(
+    expand_cyclic=[1, 2, 3],
+)
+print(stress_vm_1_2_3)
+stress_vm_1_2_3.plot()
 
-# displacement.plot()
+#############################################################################
+# Extract equivalent von mises nodal stress without expansion
+# -----------------------------------------------------------
+
+stress_vm_1 = simulation.stress_eqv_von_mises_nodal(
+    expand_cyclic=False,
+)
+print(stress_vm_1)
+stress_vm_1.plot()
