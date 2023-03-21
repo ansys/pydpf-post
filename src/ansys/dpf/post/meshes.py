87195ea0627ac86812d70eb06de9f6da95ac0228
paul.profizi <paul.profizi@ansys.com>
2023-03-21 15:57:29 +0100

    Add Meshes.select()

--- src/ansys/dpf/post/meshes.py
+++ src/ansys/dpf/post/meshes.py
@@ -6,6 +6,9 @@ Meshes
 """
 from __future__ import annotations
 
+from typing import Union
+
+import ansys.dpf.core as dpf
 from ansys.dpf.core import MeshesContainer
 
 from ansys.dpf.post.mesh import Mesh
@@ -25,3 +28,43 @@ class Meshes:
     def __str__(self):
         """String representation of this class."""
         return str(self._core_object)
+
+    def select(self, **kwargs) -> Union[Mesh, Meshes, None]:
+        """Select a specific mesh based on a combination of property values.
+
+        Parameters
+        ----------
+        **kwargs:
+            A dictionary of property names with associated values to select.
+            If values are not defined for a property, all available values are selected.
+
+        Returns
+        -------
+        A Mesh when the selection results in a unique mesh,
+        or a Meshes when several are selected,
+        or None when the criteria result in no mesh.
+
+        """
+        initial_labels = self._core_object.labels
+        # Filter labels
+        label_space = {}
+        for key in kwargs.keys():
+            if key in initial_labels:
+                label_space[key] = kwargs[key]
+        selected_meshes = self._core_object.get_meshes(label_space=label_space)
+        if len(selected_meshes) == 1:
+            return Mesh(meshed_region=selected_meshes[0])
+        elif len(selected_meshes) == 0:
+            return None
+        else:
+            meshes_container = dpf.MeshesContainer()
+            for label in initial_labels:
+                meshes_container.add_label(label=label)
+            for i, mesh in enumerate(self._core_object):
+                if mesh in selected_meshes:
+                    new_label_space = self._core_object.get_label_space(i)
+                    meshes_container.add_mesh(
+                        label_space=new_label_space,
+                        mesh=mesh,
+                    )
+            return Meshes(meshes_container=meshes_container)
