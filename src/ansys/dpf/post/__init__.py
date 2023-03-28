85957abc631a3a6621a701b4ed027412000ffafd
paul.profizi <paul.profizi@ansys.com>
2023-03-28 11:34:19 +0200

    Expose elemental_properties at post level

--- src/ansys/dpf/post/__init__.py
+++ src/ansys/dpf/post/__init__.py
@@ -27,6 +27,7 @@ except ModuleNotFoundError:  # pragma: no cover
 
 from ansys.dpf.post import mesh, selection, tools
 from ansys.dpf.post.common import Grouping as grouping
+from ansys.dpf.post.common import elemental_properties  # noqa: F401
 from ansys.dpf.post.dataframe import DataFrame  # noqa: F401
 from ansys.dpf.post.dpf_path import create_path_on_coordinates
 from ansys.dpf.post.harmonic_mechanical_simulation import (  # noqa: F401
