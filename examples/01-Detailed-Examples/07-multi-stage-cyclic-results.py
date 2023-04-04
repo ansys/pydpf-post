7d007f627d5cc46342dc53b38bdb9f8c51bdf649
paul.profizi <paul.profizi@ansys.com>
2023-04-04 10:38:40 +0200

    Force one-based indexing for sectors in cyclic

--- examples/01-Detailed-Examples/07-multi-stage-cyclic-results.py
+++ examples/01-Detailed-Examples/07-multi-stage-cyclic-results.py
@@ -27,6 +27,9 @@ from ansys.dpf.post import examples
 example_path = examples.download_multi_stage_cyclic_result()
 simulation = post.ModalMechanicalSimulation(example_path)
 
+# print the simulation to get an overview of what's available
+print(simulation)
+
 #############################################################################
 # Extract expanded displacement norm
 # ----------------------------------
@@ -42,28 +45,28 @@ displacement_norm.plot()
 # Extract equivalent von Mises nodal stress without expansion
 # -----------------------------------------------------------
 
-stress_vm_sector_0_both_stages = simulation.stress_eqv_von_mises_nodal(
+stress_vm_sector_1_both_stages = simulation.stress_eqv_von_mises_nodal(
     expand_cyclic=False,
 )
-print(stress_vm_sector_0_both_stages)
-stress_vm_sector_0_both_stages.plot()
+print(stress_vm_sector_1_both_stages)
+stress_vm_sector_1_both_stages.plot()
 
 #############################################################################
 # Extract equivalent von Mises nodal stress expanded on the first four sectors of the first stage
 # -----------------------------------------------------------------------------------------------
 
-stress_vm_sectors_0_1_2_3_first_stage = simulation.stress_eqv_von_mises_nodal(
-    expand_cyclic=[0, 1, 2, 3],
+stress_vm_sectors_1_2_3_4_first_stage = simulation.stress_eqv_von_mises_nodal(
+    expand_cyclic=[1, 2, 3, 4],
 )
-print(stress_vm_sectors_0_1_2_3_first_stage)
-stress_vm_sectors_0_1_2_3_first_stage.plot()
+print(stress_vm_sectors_1_2_3_4_first_stage)
+stress_vm_sectors_1_2_3_4_first_stage.plot()
 
 #############################################################################
 # Extract equivalent von Mises nodal stress expanded on the first two sectors of both stages
 # ------------------------------------------------------------------------------------------
 
-stress_vm_sectors_0_1_both_stages = simulation.stress_eqv_von_mises_nodal(
-    expand_cyclic=[[0, 1], [0, 1]],
+stress_vm_sectors_1_2_both_stages = simulation.stress_eqv_von_mises_nodal(
+    expand_cyclic=[[1, 2], [1, 2]],
 )
-print(stress_vm_sectors_0_1_both_stages)
-stress_vm_sectors_0_1_both_stages.plot()
+print(stress_vm_sectors_1_2_both_stages)
+stress_vm_sectors_1_2_both_stages.plot()
