e81da719186fcd0db91a6cdb0d6d2d4c7b43ccb9
Antoine Karcher <antoine.karcher@ansys.com>
2023-04-25 16:25:18 +0200

    Added mesh related tests

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -21,17 +21,72 @@ def test_mesh_node_ids(mesh):
     assert len(n_ids) == 81
     assert all([isinstance(i, (int, np.integer)) for i in n_ids])
 
-
 def test_mesh_element_ids(mesh):
     e_ids = mesh.element_ids
     assert len(e_ids) == 8
     assert all([isinstance(i, (int, np.integer)) for i in e_ids])
 
+def test_mesh_num(mesh):
+    assert mesh.num_nodes == 81
+    assert mesh.num_elements == 8
 
-def test_mesh_available_named_selections(mesh):
-    ns = mesh.available_named_selections
+def test_mesh_named_selections(mesh):
+    ns = mesh.named_selections.keys()
     assert len(ns) == 1
     assert all([isinstance(n, str) for n in ns])
+    assert ns[0] == '_FIXEDSU'
+    assert len(mesh.named_selections[ns[0]].ids) == 21
+
+def test_mesh_unit(mesh):
+    assert mesh.unit == 'm'
+
+def test_mesh_nodes(mesh):
+    node_idx_0 = mesh.nodes[0]
+    node_id_81 = mesh.nodes.by_id[81]
+
+    assert len(mesh.nodes) == 81
+    assert node_idx_0.index == 0
+    assert node_id_81.id == 81
+    assert all(np.isclose(node_idx_0.coordinates, [0.015, 0.045, 0.015]))
+    assert all(np.isclose(node_id_81.coordinates, [0.03, 0.045, 0.0075]))
+    assert np.array_equal([node.index for node in mesh.nodes], list(range(81)))
+    assert mesh.get_node_by_id(node_idx_0.id).index == 0
+
+def test_mesh_elements(mesh):
+    elem_idx_0 = mesh.elements[0]
+    elem_id_8 = mesh.elements.by_id[8]
+
+    assert len(mesh.elements) == 8
+    assert elem_idx_0.index == 0
+    assert elem_id_8.id == 8
+    assert elem_idx_0.num_nodes == 20
+    assert elem_idx_0.type == dpf.element_types.Hex20.value
+    assert elem_idx_0.shape == 'solid'
+    assert mesh.get_element_by_id(elem_idx_0.id).index == 0
+
+def test_mesh_nodal_connectivity(mesh):
+    conn_node_get_idx = mesh.node_to_element_connectivity
+    conn_node_get_id = mesh.node_to_element_ids_connectivity
+
+    node_idx_0 = mesh.nodes[0]
+    conn_idx = node_idx_0.nodal_connectivity
+    conn_id = [mesh.elements[idx].id for idx in conn_idx]
+
+    assert len(conn_idx) == len(conn_id)
+    assert np.array_equal(conn_idx, conn_node_get_idx[0])
+    assert np.array_equal(conn_id, conn_node_get_id[0])
+
+def test_mesh_elemental_connectivity(mesh):
+    conn_elem_get_idx = mesh.element_to_node_connectivity
+    conn_elem_get_id = mesh.element_to_node_ids_connectivity
+
+    elem_idx_0 = mesh.elements[0]
+    conn_idx = elem_idx_0.connectivity
+    conn_id = [mesh.nodes[idx].id for idx in conn_idx]
+    
+    assert len(conn_idx) == len(conn_id)
+    assert np.array_equal(conn_idx, conn_elem_get_idx[0])
+    assert np.array_equal(conn_id, conn_elem_get_id[0])
 
 
 def test_mesh_str(mesh):
