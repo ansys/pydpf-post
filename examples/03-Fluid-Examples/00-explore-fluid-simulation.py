be1f222ae7166f4752c52df0031c66c753368fe9
paul.profizi <paul.profizi@ansys.com>
2023-05-16 10:23:03 +0200

    WIP

--- examples/03-Fluid-Examples/00-explore-fluid-simulation.py
+++ examples/03-Fluid-Examples/00-explore-fluid-simulation.py
@@ -6,14 +6,24 @@ Explore Fluid Simulation
 This example shows how to load a fluid simulation and explore the model and its available zones,
 species, and phases.
 """
+from ansys.dpf.core.dpf_operator import available_operator_names
+
 from ansys.dpf import post
 
 # Load the fluid analysis result
 simulation = post.FluidSimulation(
     r"D:\ANSYSDev\Sandbox\plugins\Ans.Dpf.CFF\source\Ans.Dpf.CFFTest\test_models\FLPRJ\axial_comp\axial_comp_reduced.flprj"  # noqa
 )
-print(dir(simulation.result_info))
-print(simulation.result_info.available_qualifier_labels)
+server = simulation._model._server
+available_operators = available_operator_names(server=server)
+cff_operators = [operator for operator in available_operators if "cff::" in operator]
+print("CFF operators ==============================================")
+print(cff_operators)
+print("============================================================")
+
+print(simulation.result_info)
+# print(simulation.result_info.available_qualifier_labels)
+print(simulation.density())
 
 # print(simulation.mesh)
 # print(simulation.zones)
