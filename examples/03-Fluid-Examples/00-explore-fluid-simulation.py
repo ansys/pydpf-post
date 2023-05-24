860b08db78de090d0f15af6fbb0ed751c1227563
paul.profizi <paul.profizi@ansys.com>
2023-05-24 10:50:02 +0200

    Update 00-explore-fluid-simulation.py to work on GH

--- examples/03-Fluid-Examples/00-explore-fluid-simulation.py
+++ examples/03-Fluid-Examples/00-explore-fluid-simulation.py
@@ -6,26 +6,50 @@ Explore Fluid Simulation
 This example shows how to load a fluid simulation and explore the model and its available zones,
 species, and phases.
 """
+from ansys.dpf.core import examples
 from ansys.dpf.core.dpf_operator import available_operator_names
 
+from ansys.dpf import core as dpf
 from ansys.dpf import post
 
-# Load the fluid analysis result
-simulation = post.FluidSimulation(
-    r"D:\ANSYSDev\Sandbox\plugins\Ans.Dpf.CFF\source\Ans.Dpf.CFFTest\test_models\FLPRJ\axial_comp\axial_comp_reduced.flprj"  # noqa
+fluid_example_files = examples.download_fluent_files()
+ds = dpf.DataSources()
+ds.set_result_file_path(
+    fluid_example_files["cas"],
+    key="cas",
 )
-server = simulation._model._server
+ds.add_file_path(
+    fluid_example_files["dat"],
+    key="dat",
+)
+
+server = dpf.SERVER
+print(server.version)
 available_operators = available_operator_names(server=server)
 cff_operators = [operator for operator in available_operators if "cff::" in operator]
 print("CFF operators ==============================================")
 print(cff_operators)
 print("============================================================")
 
-print(simulation.result_info)
-# print(simulation.result_info.available_qualifier_labels)
-print(simulation.density())
+rip = dpf.operators.metadata.result_info_provider(data_sources=ds).eval()
+print(rip)
 
-# print(simulation.mesh)
-# print(simulation.zones)
-# print(simulation.species)
-# print(simulation.phases)
+
+# Load the fluid analysis result
+simulation = post.FluidSimulation(ds)
+print(simulation.result_info)
+
+# # print(simulation.result_info.available_qualifier_labels)  # Requires new gate
+
+density = simulation.density()
+# density_op = model.operator("RHO")
+# density = density_op.outputs.fields_container()
+print(density)
+
+velocity = simulation.velocity()
+print(velocity)
+
+# # print(simulation.mesh_info)
+# # print(simulation.zones)
+# # print(simulation.species)
+# # print(simulation.phases)
