be0229a74f2fc8a5679f27a0aec3e850a5f52666
paul.profizi <paul.profizi@ansys.com>
2023-05-24 10:47:25 +0200

    Update test_fluid_simulation.py to work on GitHub

--- tests/test_fluid_simulation.py
+++ tests/test_fluid_simulation.py
@@ -1,14 +1,30 @@
+from ansys.dpf.core import examples
 from pytest import fixture
 
+from ansys.dpf import core as dpf
 from ansys.dpf import post
 
 
 @fixture
-def fluid_example():
-    return r"D:\ANSYSDev\Sandbox\plugins\Ans.Dpf.CFF\source\Ans.Dpf.CFFTest\test_models\FLPRJ\axial_comp\axial_comp_reduced.flprj"  # noqa
+def fluid_simulation():
+    fluid_example_files = examples.download_fluent_files()
+    ds = dpf.DataSources()
+    ds.set_result_file_path(
+        fluid_example_files["cas"],
+        key="cas",
+    )
+    ds.add_file_path(
+        fluid_example_files["dat"],
+        key="dat",
+    )
+    return post.FluidSimulation(ds)  # noqa
 
 
-def test_simulation_init(fluid_example):
-    fluid_simulation = post.FluidSimulation(fluid_example)
-    print(fluid_simulation)
+def test_simulation_init(fluid_simulation):
     assert fluid_simulation is not None
+
+
+def test_density(fluid_simulation):
+    result = fluid_simulation.density()
+    assert result is not None
+    print(result)
