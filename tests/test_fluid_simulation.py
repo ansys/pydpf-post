86ab2b3190a007dd625594e2e11f4be62a26b299
paul.profizi <paul.profizi@ansys.com>
2023-05-24 11:06:49 +0200

    Mark tests as requiring DPF 6.0 for now, to be changed to 7.0

--- tests/test_fluid_simulation.py
+++ tests/test_fluid_simulation.py
@@ -1,30 +1,36 @@
 from ansys.dpf.core import examples
+from conftest import SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_6_0
+import pytest
 from pytest import fixture
 
 from ansys.dpf import core as dpf
 from ansys.dpf import post
 
 
-@fixture
-def fluid_simulation():
-    fluid_example_files = examples.download_fluent_files()
-    ds = dpf.DataSources()
-    ds.set_result_file_path(
-        fluid_example_files["cas"],
-        key="cas",
-    )
-    ds.add_file_path(
-        fluid_example_files["dat"],
-        key="dat",
-    )
-    return post.FluidSimulation(ds)  # noqa
+@pytest.mark.skipif(
+    not SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_6_0,
+    # TODO: change to SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_7_0
+    reason="Fluid capabilities added with ansys-dpf-server 2024.1.pre0.",
+)
+class TestFluidSimulation:
+    @fixture
+    def fluid_simulation(self):
+        fluid_example_files = examples.download_fluent_files()
+        ds = dpf.DataSources()
+        ds.set_result_file_path(
+            fluid_example_files["cas"],
+            key="cas",
+        )
+        ds.add_file_path(
+            fluid_example_files["dat"],
+            key="dat",
+        )
+        return post.FluidSimulation(ds)  # noqa
 
+    def test_simulation_init(self, fluid_simulation):
+        assert fluid_simulation is not None
 
-def test_simulation_init(fluid_simulation):
-    assert fluid_simulation is not None
-
-
-def test_density(fluid_simulation):
-    result = fluid_simulation.density()
-    assert result is not None
-    print(result)
+    def test_density(self, fluid_simulation):
+        result = fluid_simulation.density()
+        assert result is not None
+        print(result)
