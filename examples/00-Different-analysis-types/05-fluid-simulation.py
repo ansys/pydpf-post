11d675a1d0a48cf71cf5b6ebd0d59a72c9ba169d
paul.profizi <paul.profizi@ansys.com>
2023-05-24 10:55:20 +0200

    Delete .md in this branch, update 05-fluid-simulation.py to work on GH

--- examples/00-Different-analysis-types/05-fluid-simulation.py
+++ examples/00-Different-analysis-types/05-fluid-simulation.py
@@ -6,41 +6,31 @@ Fluid Simulation
 This example shows how to load a fluid simulation and extract results like velocity and pressure.
 It also shows extraction and selection of results by zone, phase or species.
 """
-
-import ansys.dpf.core as dpf
-from ansys.dpf.core.dpf_operator import available_operator_names
-
 ###############################################################################
 # Perform required imports
 # ------------------------
+from ansys.dpf import core as dpf
 from ansys.dpf import post
 from ansys.dpf.post import examples
 
-# Start a server and check its version
-server = dpf.start_local_server()
-print(f"{server.version=}")
-# # Check CFF operators are now available
-# available_operators = available_operator_names(server=server)
-# cff_operators = [operator for operator in available_operators if "cff::" in operator]
-# print("CFF operators ==============================================")
-# print(cff_operators)
-# print("============================================================")
-
 # Load the fluid analysis result
-simulation = post.FluidSimulation(
-    r"D:\ANSYSDev\Sandbox\plugins\Ans.Dpf.CFF\source\Ans.Dpf.CFFTest\test_models\FLPRJ\axial_comp\axial_comp_reduced.flprj"  # noqa
+fluid_example_files = examples.download_fluent_files()
+ds = dpf.DataSources()
+ds.set_result_file_path(
+    fluid_example_files["cas"],
+    key="cas",
 )
+ds.add_file_path(
+    fluid_example_files["dat"],
+    key="dat",
+)
+
+simulation = post.FluidSimulation(ds)
 print(simulation)
 
 mesh = simulation.mesh
 print(mesh)
 # mesh.plot()
-
-available_operators = available_operator_names(server=server)
-cff_operators = [operator for operator in available_operators if "cff::" in operator]
-print("CFF operators ==============================================")
-print(cff_operators)
-print("============================================================")
 
 # Explore the structure of the results
 # print(simulation.zones)
