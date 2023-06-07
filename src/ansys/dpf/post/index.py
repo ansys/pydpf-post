72f35b0f601c75889d0873bccd01c08967cc90ca
paul.profizi <paul.profizi@ansys.com>
2023-06-07 16:25:59 +0200

    Revert "Add per-Index data format"
    This reverts commit 7f7e051cb8149b5bb481e55f04d310bbabc95d4a.

--- src/ansys/dpf/post/index.py
+++ src/ansys/dpf/post/index.py
@@ -64,7 +64,6 @@ class Index(ABC):
         self._dtype = None
         self._len = None
         self._scoping_ref = None
-        self._data_fmt = None
         # if scoping is None and values is None:
         #     raise ValueError("Arguments 'values' and 'scoping' cannot both be None.")
         if scoping is not None:
@@ -113,16 +112,6 @@ class Index(ABC):
         """Evaluates the values of the Index."""
         if self._scoping_ref is not None:
             self._values = self._scoping_ref().ids
-
-    @property
-    def data_format(self):
-        """Gets the data string formatting for this Index."""
-        return self._data_fmt
-
-    @data_format.setter
-    def data_format(self, fmt: str):
-        """Sets the data string formatting for this Index."""
-        self._data_fmt = fmt
 
 
 class MeshIndex(Index):
