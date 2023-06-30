cb8e39cee6201475f9c4ba796aacd5537a503556
paul.profizi <paul.profizi@ansys.com>
2023-06-30 12:18:29 +0200

    Add basic fluid example

--- examples/00-Different-analysis-types/05-fluid-simulation.py
+++ examples/00-Different-analysis-types/05-fluid-simulation.py
@@ -4,39 +4,75 @@
 Fluid Simulation
 ================
 This example shows how to load a fluid simulation and extract results like velocity and pressure.
-It also shows extraction and selection of results by zone, phase or species.
+It then shows extraction and selection of results by zone, phase or species.
 """
 ###############################################################################
 # Perform required imports
 # ------------------------
-from ansys.dpf import core as dpf
 from ansys.dpf import post
 from ansys.dpf.post import examples
 
+###############################################################################
 # Load the fluid analysis result
-fluid_example_files = examples.download_fluent_files()
-ds = dpf.DataSources()
-ds.set_result_file_path(
-    fluid_example_files["cas"],
-    key="cas",
-)
-ds.add_file_path(
-    fluid_example_files["dat"],
-    key="dat",
-)
+# ------------------------------
+fluid_example_files = examples.download_fluent_axial_comp()
 
-simulation = post.FluidSimulation(ds)
+simulation = post.FluidSimulation(
+    cas=fluid_example_files["cas"], dat=fluid_example_files["dat"]
+)
+# Printing the simulation will show most of the available metadata
 print(simulation)
 
-# mesh = simulation.mesh
-# print(mesh)
-# mesh.plot()
 
-# Explore the structure of the results
+###############################################################################
+# Explore the available structure
+# -------------------------------
+# Check the available cell and face zones
 # print(simulation.zones)
-# print(simulation.species)
-# print(simulation.phases)
 
+###############################################################################
+# Check the available species
+print(simulation.species)
+
+###############################################################################
+# Check the available phases
+print(simulation.phases)
+
+
+###############################################################################
 # Extract a result
-density = simulation.density(zone_ids=[-1], phases=[1])
-print(density)
+# ----------------
+###############################################################################
+# Check the metadata on available results
+print(simulation.result_info)
+
+###############################################################################
+# You can also get a direct list of ``AvailableResult`` objects
+print(simulation.results)
+# Print a specific one to get more information on available qualifiers (zones, phases and so on)
+print(simulation.results[0])
+
+###############################################################################
+# Extract this available result as a Dataframe
+print(simulation.enthalpy())
+# Not specifying any qualifier returns a unique column of data
+
+###############################################################################
+# Available qualifiers for this result can be used in the extraction request
+# to filter/separate data.
+# Here we want a different column for each available zone for this result
+print(simulation.enthalpy(zone_ids=[13, 28]))
+# Here only the data for the fluid-stator zone is extracted
+print(simulation.enthalpy(zone_ids=[28]))
+# The same logic can be applied to any available qualifier found in the ``AvailableResult``
+# description under 'Available qualifier labels'.
+
+###############################################################################
+# The result extraction request can also contain selection arguments
+# The enthalpy result being defined on cells, you can request data for specific
+# cells using their IDs:
+print(simulation.enthalpy(cell_ids=[1, 2]))
+
+###############################################################################
+# For selection and manipulation of the Dataframe,
+# please refer to example "Create and manipulate a DPF Dataframe"
