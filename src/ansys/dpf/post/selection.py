8725827524c97d66e952c890438a0ada8516329d
paul.profizi <paul.profizi@ansys.com>
2023-07-11 14:22:35 +0200

    Coverage selection.py

--- src/ansys/dpf/post/selection.py
+++ src/ansys/dpf/post/selection.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, List
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from ansys.dpf.post.simulation import Simulation
     from ansys.dpf.post.mesh import Mesh
 
