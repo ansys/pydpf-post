2411ecdbd762a9a9603e544c7c4ca40a92434e29
paul.profizi <paul.profizi@ansys.com>
2023-08-01 16:41:54 +0200

    Remove rogue prints from test

--- tests/test_fluid_simulation.py
+++ tests/test_fluid_simulation.py
@@ -110,12 +110,12 @@ class TestFluidSimulation:
         assert isinstance(fluent_simulation.phases, PhasesDict)
 
     def test_fluid_simulation_result_unavailable(self, fluent_simulation):
-        print(fluent_simulation)
+        # print(fluent_simulation)
         with pytest.raises(ValueError, match="is not available."):
             _ = fluent_simulation.wall_shear_stress()
 
     def test_results_fluent_averaging_from_elemental(self, fluent_simulation):
-        print(fluent_simulation)
+        # print(fluent_simulation)
         # ######## Elemental Result #################
         # Request on None
         result = fluent_simulation.enthalpy()
@@ -145,7 +145,7 @@ class TestFluidSimulation:
         assert result._core_object[0].location == post.locations.elemental
 
     def test_results_fluent_averaging_from_elemental_faces(self, fluent_simulation):
-        print(fluent_simulation)
+        # print(fluent_simulation)
         # ######## ElementalFaces Result #################
         # Request on None
         result = fluent_simulation.static_pressure()
