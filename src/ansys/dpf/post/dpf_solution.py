6cb9fa6a532f8cc432991f33417fa91d2d44357c
Camille Bellot <80476446+cbellot000@users.noreply.github.com>
2023-10-04 14:50:08 +0200

    Update dpf_solution.py

--- src/ansys/dpf/post/dpf_solution.py
+++ src/ansys/dpf/post/dpf_solution.py
@@ -77,7 +77,7 @@ class DpfSolution:
              -  co_energy: Elemental co-energy
              -  incremental_energy: Elemental incremental energy
              -  elastic_strain: ElementalNodal Strain
-             -  element_euler_angles: ElementalNodal EUL
+             -  element_euler_angles: ElementalNodal Element Euler Angles
              -  structural_temperature: ElementalNodal Structural temperature
         """
         return self._model.metadata.result_info
