9cb8fa4306457cc8380efab07ad22b85627f4761
paul.profizi <paul.profizi@ansys.com>
2024-05-03 10:35:27 +0200

    Add possibility to get zone mesh by name

--- src/ansys/dpf/post/fluid_simulation.py
+++ src/ansys/dpf/post/fluid_simulation.py
@@ -178,13 +178,26 @@ class FluidSimulation(Simulation):
             self._mesh_info = FluidMeshInfo(self._model.metadata.mesh_info)
         return self._mesh_info
 
-    def zone_mesh(self, zone_id: int) -> Mesh:
-        """Return the mesh of the given zone."""
-        if zone_id not in self.cell_zones.keys():
-            if zone_id not in self.face_zones.keys():
-                raise ValueError(f"'{zone_id}' is not a valid zone ID.")
+    def zone_mesh(self, zone: Union[int, str]) -> Mesh:
+        """Return the mesh of the given zone (ID or name)."""
+        if isinstance(zone, int):
+            if zone not in self.cell_zones.keys():
+                if zone not in self.face_zones.keys():
+                    raise ValueError(f"'{zone}' is not a valid zone ID.")
+        elif isinstance(zone, str):
+            zone_id_found = None
+            for zone_id, zone_name in self.cell_zones.items():
+                if zone == zone_name:
+                    zone_id_found = zone_id
+            if zone_id_found is None:
+                for zone_id, zone_name in self.face_zones.items():
+                    if zone == zone_name:
+                        zone_id_found = zone_id
+                if zone_id_found is None:
+                    raise ValueError(f"'{zone}' is not a valid zone name.")
+            zone = int(zone_id_found)
         mesh_provider = self._model.metadata.mesh_provider
-        mesh_provider.inputs.region_scoping(zone_id)
+        mesh_provider.inputs.region_scoping(zone)
         return Mesh(mesh_provider.eval())
 
     @property
