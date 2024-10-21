04ea1e323b8ca7b08d1e7b43fad2610ac1617cdc
PProfizi <PProfizi@users.noreply.github.com>
2024-10-21 13:33:38 +0000

    DOC: update development documentation 81b9514d4f80c9bd36d1edf8ad6dce97a54857b6

---
+++ version/0.3/_downloads/ece6dec770cac9c49169335af4a675a6/03-modal-analysis.py
@@ -0,0 +1,148 @@
+"""
+.. _ref_modal_analysis:
+
+Modal analysis
+==============
+This example shows how you can post-process the result file for a modal analysis
+using PyDPF-Post.
+"""
+
+###############################################################################
+# Perform required imports
+# ------------------------
+# Perform required imports.
+
+from ansys.dpf import post
+from ansys.dpf.post import examples
+
+###############################################################################
+# Get ``Solution`` object
+# -----------------------
+# Get the ``Solution`` object. This example loads a result file for a modal analysis
+# computed in Ansys Mechanical.
+
+example_path = examples.download_all_kinds_of_complexity_modal()
+
+solution = post.load_solution(example_path)
+print(solution)
+
+
+###############################################################################
+# Get ``Result`` objects
+# ----------------------
+
+###############################################################################
+# Get displacement result
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# Get the displacement ``Result`` object. It contains a field for real values
+# and a field for imaginary values.
+
+disp_result = solution.displacement()
+disp = disp_result.vector
+
+###############################################################################
+# Check number of fields
+# ~~~~~~~~~~~~~~~~~~~~~~
+# Check the number of fields.
+
+disp.num_fields
+
+###############################################################################
+# **Get data from a field**
+
+disp.get_data_at_field(0)
+
+###############################################################################
+# Get maximum data value over all fields
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get the maximum data value over all fields.
+
+disp.max_data
+
+###############################################################################
+# Get minimum data value over all fields
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get the minimum data value over all fields.
+
+disp.min_data
+
+###############################################################################
+# Get maximum data value over targeted field
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get the maximum data value over a targeted field.
+
+disp.get_max_data_at_field(0)
+
+###############################################################################
+# Get minimum data value over all fields
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get the minimum data value over all fields.
+
+disp.get_min_data_at_field(0)
+
+###############################################################################
+# Get stress result
+# -----------------
+# Get a stress result that deals with amplitude. It contains a field for real
+# values and a field for imaginary values.
+
+stress_result = solution.stress()
+
+###############################################################################
+# Check if support has complex frequencies
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Check if the support has complex frequencies.
+
+stress_result.has_complex_frequencies()
+
+###############################################################################
+# Get tensor result
+# ~~~~~~~~~~~~~~~~~
+# Get the tensor result.
+
+stress = stress_result.tensor
+stress.num_fields
+
+###############################################################################
+# Get shell field
+# ~~~~~~~~~~~~~~~
+# Get the shell field.
+
+shell_field = stress[0]
+shell_field.shell_layers
+
+###############################################################################
+# Get solid field
+# ~~~~~~~~~~~~~~~
+# Get the solid field.
+
+solid_field = stress[1]
+
+###############################################################################
+# Plot amplitude contour
+# ~~~~~~~~~~~~~~~~~~~~~~
+# Plot the amplitude contour.
+
+amplitude = stress_result.tensor_amplitude
+stress.plot_contour()
+
+###############################################################################
+# Get elastic strain result
+# =========================
+# Get an elastic strain result that deals with phase. It contains a field for
+# real values and a field for imaginary values.
+
+elastic_strain_result = solution.elastic_strain()
+elastic_strain = elastic_strain_result.tensor
+
+# Check number of fields
+# ~~~~~~~~~~~~~~~~~~~~~~
+# Check the number of shell and solid elements in distinct fields.
+
+elastic_strain.num_fields
+
+###############################################################################
+# If the result file contains results, you can use this method
+# to get the elastic strain result.
+
+print(solution.plastic_strain())
---
+++ version/0.5/_downloads/b44280b46374b90d5107e84343601e13/04-invariants.py
@@ -0,0 +1,74 @@
+"""
+.. _ref_invariants_example:
+
+Extract stress/strain invariants - Static Simulation
+=====================================================================
+In this script a static simulation is used as an example to show how to
+extract tensor's invariants.
+"""
+
+###############################################################################
+# Perform required imports
+# ------------------------
+# Perform required imports. # This example uses a supplied file that you can
+# get by importing the DPF ``examples`` package.
+
+from ansys.dpf import post
+from ansys.dpf.post import examples
+
+###############################################################################
+# Get ``Simulation`` object
+# -------------------------
+# Get the ``Simulation`` object that allows access to the result. The ``Simulation``
+# object must be instantiated with the path for the result file. For example,
+# ``"C:/Users/user/my_result.rst"`` on Windows or ``"/home/user/my_result.rst"``
+# on Linux.
+
+example_path = examples.download_crankshaft()
+# to automatically detect the simulation type, use:
+simulation = post.load_simulation(example_path)
+
+# to enable auto-completion, use the equivalent:
+simulation = post.StaticMechanicalSimulation(example_path)
+
+# print the simulation to get an overview of what's available
+print(simulation)
+
+###############################################################################
+# Extract elemental nodal stress and strain
+# -----------------------------------------
+
+stress = simulation.stress(all_sets=True)
+print(stress)
+
+strain = simulation.elastic_strain(all_sets=True)
+print(strain)
+
+###############################################################################
+# Compute principal invariant averaged and unaveraged on stress and strain
+# ------------------------------------------------------------------------
+
+princ_stress_1 = simulation.stress_principal(components=[1])
+print(princ_stress_1)
+
+nodal_princ_stress_2 = simulation.stress_principal_nodal(components=[2])
+print(nodal_princ_stress_2)
+nodal_princ_stress_2.plot()
+
+nodal_princ_strain_2 = simulation.elastic_strain_principal_nodal(components=[2])
+print(nodal_princ_strain_2)
+nodal_princ_strain_2.plot()
+
+
+###############################################################################
+# Compute von Mises eqv averaged and unaveraged on stress and strain
+# ------------------------------------------------------------------------
+
+stress_eqv = simulation.stress_eqv_von_mises(set_ids=[1])
+print(stress_eqv)
+
+nodal_stress_eqv = simulation.stress_eqv_von_mises_nodal(set_ids=[1])
+nodal_stress_eqv.plot()
+
+nodal_strain_eqv = simulation.elastic_strain_eqv_von_mises_nodal(set_ids=[1])
+nodal_strain_eqv.plot()
