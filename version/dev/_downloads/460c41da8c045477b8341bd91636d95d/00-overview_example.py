57c3cfad9bed650073954803b19482c257792a0b
Sangsimon <Sangsimon@users.noreply.github.com>
2026-01-16 14:17:06 +0000

    DOC: update development documentation 4137abfdff916c5fe982efea26d5cb41d2a8758a

---
+++ version/dev/_downloads/460c41da8c045477b8341bd91636d95d/00-overview_example.py
@@ -0,0 +1,119 @@
+# Copyright (C) 2020 - 2026 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+.. _ref_overview_example:
+
+Use basic features
+==================
+This example shows how to use basic features of the legacy PyDPF-Post API.
+"""
+
+###############################################################################
+# Perform required imports
+# ------------------------
+# Perform required imports. This example uses a supplied file that you can
+# get by importing the DPF ``examples`` package.
+
+from ansys.dpf import post
+from ansys.dpf.post import examples
+
+###############################################################################
+# Get ``Solution`` object
+# -----------------------
+# Get the ``Solution`` object. This example loads a simple file supplied with
+# PyDPF-Post.
+
+solution = post.load_solution(examples.multishells_rst)
+
+###############################################################################
+# Get mesh and time frequency support
+# -----------------------------------
+# Get the mesh and time frequency support. The mesh is the support of the model.
+# The time frequency support is the time/frequency representation of the model.
+
+mesh = solution.mesh
+time_freq_support = solution.time_freq_support
+
+###############################################################################
+# Get ``Result`` object
+# ---------------------
+# Get a ``Result`` object from the ``Solution`` object. The ``Result`` object
+# can be a stress, plastic strain, elastic strain, temperature, or displacement.
+
+post.print_available_keywords()
+stress = solution.stress(location=post.locations.nodal, time_scoping=[1])
+
+# Both ''location'' and ``time_scoping`` are available in the definition of
+# the stress result.
+
+stress.definition.location
+stress.definition.time_scoping
+
+print(stress)
+
+###############################################################################
+# Compute data
+# ------------
+# **SX subresult**
+#
+# This code gets the ``SX`` subresult, which is the stress tensor in the XX direction.
+
+sx = stress.xx
+sx.num_fields
+sx_field = sx[0]
+sx_data = sx.get_data_at_field(0)
+print("Length of the data:", len(sx_data))
+print()
+print("Maximum Stress Field:\n", sx.max)
+print()
+print("Maximum data at stress field:", sx.max_data)
+print("Maximum SX at Field 0:", sx.get_max_data_at_field(0))
+
+###############################################################################
+# **Stress tensor result**
+#
+# This code gets the minimum and maximum stresses at a field for all
+# directions (``XX``, ``XY``, ``XZ``, ``XY``, ``YZ``, and ``XZ``.
+
+s = stress.tensor
+s_field = s[0]
+s_data = sx.get_data_at_field(0)
+print("Length of the data:", len(s_data))
+print()
+print("Maximum stress field:\n", s.max)
+print()
+print("Maximum data at stress field:", s.max_data)
+print("Maximum stress tensors at field 0:\n", s.get_max_data_at_field(0))
+
+print("Minimum stress field:\n", s.min)
+print()
+print("Minimum data at stress field:", s.min_data)
+print("Minimum stress tensors at field 0:\n", s.get_min_data_at_field(0))
+
+
+###############################################################################
+# Plot result
+# -----------
+# Plot a result by using the ``plot_contour()`` method.
+
+s.plot_contour()
---
+++ version/stable/_downloads/392eecf2e169a9752a3af7871bb63825/03-modal-analysis.py
@@ -0,0 +1,162 @@
+# Copyright (C) 2020 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+.. _ref_modal_analysis:
+
+Postprocess the result file for a modal analysis
+================================================
+This example shows how to use the legacy PyDPF-Post API to postprocess the result
+file for a modal analysis.
+"""
+
+###############################################################################
+# Perform required imports
+# ------------------------
+# Perform required imports. This example uses a supplied file that you can
+# get by importing the DPF ``examples`` package.
+
+from ansys.dpf import post
+from ansys.dpf.post import examples
+
+###############################################################################
+# Get ``Solution`` object
+# -----------------------
+# Get the ``Solution`` object. This example loads a result file for a modal analysis
+# computed in Ansys Mechanical.
+
+example_path = examples.download_all_kinds_of_complexity_modal()
+
+solution = post.load_solution(example_path)
+print(solution)
+
+
+###############################################################################
+# Get ``Result`` objects
+# ----------------------
+
+###############################################################################
+# Get displacement result
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# The displacement result contains a field for real values and a field for
+# imaginary values.
+
+disp_result = solution.displacement()
+disp = disp_result.vector
+
+###############################################################################
+# Get number of fields
+# ~~~~~~~~~~~~~~~~~~~~~~
+
+disp.num_fields
+
+###############################################################################
+# Get data from a field
+# ~~~~~~~~~~~~~~~~~~~~~
+
+disp.get_data_at_field(0)
+
+###############################################################################
+# Get maximum data value over all fields
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+disp.max_data
+
+###############################################################################
+# Get minimum data value over all fields
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+disp.min_data
+
+###############################################################################
+# Get maximum data value over targeted field
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+disp.get_max_data_at_field(0)
+
+###############################################################################
+# Get minimum data value over all fields
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+disp.get_min_data_at_field(0)
+
+###############################################################################
+# Get stress result
+# ~~~~~~~~~~~~~~~~~
+# Get a stress result that deals with amplitude. It contains a field for real
+# values and a field for imaginary values.
+
+stress_result = solution.stress()
+
+###############################################################################
+# Check if support has complex frequencies
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+stress_result.has_complex_frequencies()
+
+###############################################################################
+# Get tensor result
+# ~~~~~~~~~~~~~~~~~
+
+stress = stress_result.tensor
+stress.num_fields
+
+###############################################################################
+# Get shell field
+# ~~~~~~~~~~~~~~~
+
+shell_field = stress[0]
+shell_field.shell_layers
+
+###############################################################################
+# Get solid field
+# ~~~~~~~~~~~~~~~
+
+solid_field = stress[1]
+
+###############################################################################
+# Plot amplitude contour
+# ~~~~~~~~~~~~~~~~~~~~~~
+
+amplitude = stress_result.tensor_amplitude
+stress.plot_contour()
+
+###############################################################################
+# Get elastic strain result
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get an elastic strain result that deals with phase. It contains a field for
+# real values and a field for imaginary values.
+
+elastic_strain_result = solution.elastic_strain()
+elastic_strain = elastic_strain_result.tensor
+
+# Get number of fields
+# ~~~~~~~~~~~~~~~~~~~~~~
+# Get the number of shell and solid elements in distinct fields.
+
+elastic_strain.num_fields
+
+###############################################################################
+# If the result file contains results, use this method
+# to get the elastic strain result.
+
+print(solution.plastic_strain())
