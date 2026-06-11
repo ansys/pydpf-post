2c9343048be6dc301773d0484ef20df0e7c92324
Paul Profizi <100710998+PProfizi@users.noreply.github.com>
2026-06-11 16:02:47 +0200

    fix: version requirement conditional for examples in doc build (#1014)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,6 +6,7 @@ import os
 from ansys.dpf.core import server, server_factory
 from ansys_sphinx_theme import ansys_favicon, get_version_match, pyansys_logo_black
 import numpy as np
+from packaging.version import Version as PkgVersion
 import pyvista
 
 from ansys.dpf.post import __version__
@@ -45,13 +46,17 @@ server_instance = server.start_local_server(
 )
 server_version = server_instance.version
 server.shutdown_all_session_servers()
-print(f"DPF version: {server_version}")
+print("".rjust(40, "*"))
+print(
+    f"Doc built for DPF server version {server_version} at:\n{server_instance.ansys_path}"
+)
+print("".rjust(40, "*"))
 
 # Build ignore pattern
 ignored_pattern = r"(ignore"
 for example in glob(r"../../examples/**/*.py"):
     minimum_version_str = get_example_required_minimum_dpf_version(example)
-    if float(server_version) - float(minimum_version_str) < -0.05:
+    if PkgVersion(server_version) < PkgVersion(minimum_version_str):
         example_name = example.split(os.path.sep)[-1]
         print(
             f"Example {example_name} skipped as it requires DPF {minimum_version_str}."
